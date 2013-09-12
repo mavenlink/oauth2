7018e7af66553c7b7bcf33b96d23eebd202482dd
Jeff Moore and Roger Neel <pair+jeff+roger@mavenlink.com>
2013-09-12 13:07:31 -0700

    Fixing assertion strategy to accept claim key overrides, allowing grant type overrides and removing assertion_type and scope from headers

--- spec/oauth2/strategy/assertion_spec.rb
+++ spec/oauth2/strategy/assertion_spec.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 describe OAuth2::Strategy::Assertion do
   let(:client) do
-    cli = OAuth2::Client.new('abc', 'def', :site => 'http://api.example.com')
+    cli = OAuth2::Client.new('', '', :site => 'http://api.example.com')
     cli.connection.build do |b|
       b.adapter :test do |stub|
         stub.post('/oauth/token') do |env|
@@ -18,13 +18,89 @@ describe OAuth2::Strategy::Assertion do
     cli
   end
 
-  let(:params) { {:hmac_secret => 'foo'}}
+  let(:params) { {:hmac_secret => 'foo'} }
 
   subject {client.assertion}
 
   describe "#authorize_url" do
     it "raises NotImplementedError" do
       expect{subject.authorize_url}.to raise_error(NotImplementedError)
+    end
+  end
+
+  context "when putting together a request" do
+    before do
+      @key = OpenSSL::PKey::RSA.new(1024)
+      @params = {:iss => 'google_api_account_name@developer.gserviceaccount.com',
+                 :scope => 'https://www.googleapis.com/auth/calendar',
+                 :aud => 'https://accounts.google.com/o/oauth2/token',
+                 :exp => Time.now.to_i + 3600,
+                 :iat => Time.now.to_i,
+                 :private_key => @key}
+    end
+
+    describe "#build_request" do
+      it "assembles the request from the given params" do
+        request = subject.build_request(@params)
+        expect(request[:grant_type]).to eq('urn:ietf:params:oauth:grant-type:jwt-bearer')
+        expect(request.keys).to match_array [:grant_type, :assertion, "client_id", "client_secret"]
+      end
+    end
+
+    describe "#build_assertion" do
+      it "applies the parameters and constructs the claims" do
+        assertion = subject.build_assertion(@params)
+
+        claim_keys = %w(iss scope aud exp iat)
+        jwt = JWT.decode(assertion, @key.public_key)
+        expect(jwt.keys).to match_array(claim_keys)
+        claim_keys.each do |claim_key|
+          expect(jwt[claim_key]).to eq(@params[claim_key.to_sym])
+        end
+      end
+
+      it "optionally applies the prn param" do
+        assertion = subject.build_assertion(@params.merge!(:prn => 'swayze@roadhouse.com'))
+
+        claim_keys = %w(iss scope aud exp iat prn)
+        jwt = JWT.decode(assertion, @key.public_key)
+        expect(jwt.keys).to match_array(claim_keys)
+        claim_keys.each do |claim_key|
+          expect(jwt[claim_key]).to eq(@params[claim_key.to_sym])
+        end
+      end
+
+      describe "JWT encoding" do
+        it "encodes as HS256 when hmac_secret is passed in" do
+          @params.merge!(:hmac_secret => 'thisiskey')
+          @params.delete(:private_key)
+
+          expect(@params[:private_key]).to_not be
+          expect(@params[:hmac_secret]).to be
+
+          assertion = subject.build_assertion(@params)
+          header = MultiJson.decode(JWT.base64url_decode(assertion.split('.').first))
+          expect(header['alg']).to eq('HS256')
+        end
+
+        it "encodes as RS256 when private_key is passed in" do
+          expect(@params[:private_key]).to be
+          expect(@params[:hmac_secret]).to_not be
+
+          assertion = subject.build_assertion(@params)
+          header = MultiJson.decode(JWT.base64url_decode(assertion.split('.').first))
+          expect(header['alg']).to eq('RS256')
+        end
+
+        it "raises an argument error when nothing is passed in" do
+          @params.delete(:private_key)
+
+          expect(@params[:private_key]).to_not be
+          expect(@params[:hmac_secret]).to_not be
+
+          expect(lambda{ subject.build_assertion(@params) }).to raise_error(ArgumentError, /hmac_secret or private_key/)
+        end
+      end
     end
   end
 
@@ -52,6 +128,4 @@ describe OAuth2::Strategy::Assertion do
       end
     end
   end
-
 end
-
